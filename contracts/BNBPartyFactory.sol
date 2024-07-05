95a98eab6d3a16258aaf4e374abc48a8300423f3
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-07-05 13:59:50 +0300

    preparing for the party

--- contracts/BNBPartyFactory.sol
+++ contracts/BNBPartyFactory.sol
@@ -4,36 +4,44 @@ pragma solidity ^0.8.0;
 import "./token/ERC20Token.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@pancakeswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-import "./PoolCreation.sol";
+import "./interfaces/INonfungiblePositionManager.sol";
 
-contract BNBPartyFactory is PoolCreation {
-    address public immutable internalRouter;
-    ISwapRouter public immutable swapRouter;
+contract BNBPartyFactory {
+    INonfungiblePositionManager public immutable BNBPositionManager;
+    INonfungiblePositionManager public immutable positionManager;
+
     uint256 public immutable buyLimit;
     uint256 public immutable initialTokenAmount;
+    uint24 public immutable fee;
 
-    event TokenCreated(
+    event StartParty(
         address indexed tokenAddress,
-        string name,
-        string symbol
+        address indexed owner,
+        address indexed FLPAddress
     );
 
     constructor(
-        address _internalRouter,
-        address _swapRouter,
-        address _positionManager,
+        INonfungiblePositionManager _BNBPositionManager,
+        INonfungiblePositionManager _positionManager,
         uint24 _fee,
         uint256 _buyLimit,
-        uint256 _initialTokenAmount,
-        uint160 _sqrtPriceLimitX96
+        uint256 _initialTokenAmount
     ) {
-        internalRouter = _internalRouter;
-        swapRouter = ISwapRouter(_swapRouter);
-        positionManager = INonfungiblePositionManager(_positionManager);
+        require(
+            address(_BNBPositionManager) != address(0),
+            "BNBPositionManager is zero address"
+        );
+        require(
+            address(_positionManager) != address(0),
+            "positionManager is zero address"
+        );
+        require(_buyLimit > 0, "buyLimit is zero");
+        require(_initialTokenAmount > 0, "initialTokenAmount is zero");
+        BNBPositionManager = _BNBPositionManager;
+        positionManager = _positionManager;
         fee = _fee;
         buyLimit = _buyLimit;
         initialTokenAmount = _initialTokenAmount;
-        sqrtPriceLimitX96 = _sqrtPriceLimitX96;
     }
 
     function createToken(
@@ -42,7 +50,11 @@ contract BNBPartyFactory is PoolCreation {
     ) public payable returns (IERC20 newToken) {
         require(msg.value >= fee, "Insufficient BNB for fee");
         newToken = new ERC20Token(name, symbol, initialTokenAmount);
-        emit TokenCreated(address(newToken), name, symbol);
-        _addInitialLiquidity(newToken, initialTokenAmount);
+        // emit StartParty(address(newToken), msg.sender, address(FLP));
+        // create BNB party LP
+    }
+
+    function handleSwap() external {
+        // handle swap
     }
 }
