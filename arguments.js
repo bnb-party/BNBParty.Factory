a550997f8c493b3d031450857bc931219624d8e6
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-07-22 10:56:19 +0300

    update deploy script

--- arguments.js
+++ arguments.js
@@ -1,14 +1,24 @@
-// for bnb-party verification  
+// for bnb-party verification
+
+const partyTarget = ethers.parseEther("100")
+const tokenCreationFee = ethers.parseUnits("1", 16)
+const returnFeeAmount = ethers.parseUnits("1", 16)
+const bonusFee = ethers.parseUnits("1", 16)
+const initialTokenAmount = "10000000000000000000000000"
+const sqrtPriceX96 = "25052911542910170730777872"
+const tWBNB = "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd"
 module.exports = [
-    100000000000000000000n,
-    100000000000000000n,
-    10000,
-    10000,
-    "10000000000000000000000000",
-    "25052911542910170730777872",
-    "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",
-    100000000000000000n,
-    10000000000000000n,
-    "-92200",
-    "0"
-  ];
+    {
+        partyTarget: partyTarget,
+        createTokenFee: tokenCreationFee,
+        partyLpFee: 10000,
+        lpFee: 10000,
+        initialTokenAmount: initialTokenAmount,
+        sqrtPriceX96: sqrtPriceX96,
+        bonusTargetReach: returnFeeAmount,
+        bonusPartyCreator: bonusFee,
+        tickLower: "-92200",
+        tickUpper: "0",
+    },
+    tWBNB,
+]
