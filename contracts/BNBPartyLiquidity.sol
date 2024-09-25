e0d65b665853f0e2d1dae5641bfd6e11dd374f90
Andrew Dmytrenko <68740472+YouStillAlive@users.noreply.github.com>
2024-09-25 17:47:39 +0300

    handleLiquidity refactor 1639710081 (#79)
    * handleLiquidity refactor
    
    * simplify second lp creation
    
    * refactor handleLiquidity

--- contracts/BNBPartyLiquidity.sol
+++ contracts/BNBPartyLiquidity.sol
@@ -14,38 +14,16 @@ abstract contract BNBPartyLiquidity is BNBPartyLiquidityHelper {
     function _handleLiquidity(address recipient) internal returns (address liquidityPool, uint256 tokenId){
         IUniswapV3Pool pool = IUniswapV3Pool(msg.sender);
         (uint160 sqrtPriceX96, , , , , , ) = pool.slot0();
-        address token0 = pool.token0();
-        address token1 = pool.token1();
+        (address token0, address token1) = (pool.token0(), pool.token1());
         uint128 liquidity = pool.liquidity();
         uint256 unwrapAmount = party.bonusTargetReach + party.bonusPartyCreator + party.targetReachFee;
         uint160 newSqrtPriceX96;
-
         // Decrease liquidity and collect tokens
         (uint256 amount0, uint256 amount1) = _decreaseAndCollect(lpToTokenId[msg.sender], liquidity);
-
-        if (token0 == address(WBNB)) {
-            amount0 -= unwrapAmount; // Deduct unwrap amount from token0 if it is WBNB
-            isTokenTargetReached[token1] = true;
-            newSqrtPriceX96 = sqrtPriceCalculator.getNextSqrtPriceFromAmount0RoundingUp(
-                sqrtPriceX96,
-                liquidity,
-                unwrapAmount,
-                false
-            );
-        } else {
-            amount1 -= unwrapAmount; // Deduct unwrap amount from token1 if it is WBNB
-            isTokenTargetReached[token0] = true;
-            newSqrtPriceX96 = sqrtPriceCalculator.getNextSqrtPriceFromAmount1RoundingDown(
-                sqrtPriceX96,
-                liquidity,
-                unwrapAmount,
-                false
-            );
-        }
-
+        // Calculate new amounts and price
+        (newSqrtPriceX96, amount0, amount1) = _calculateAmountsAndPrice(token0, token1, amount0, amount1, unwrapAmount, sqrtPriceX96, liquidity);
         // Approve tokens for the new liquidity pool creation
-        IERC20(token0).safeIncreaseAllowance(address(positionManager), amount0);
-        IERC20(token1).safeIncreaseAllowance(address(positionManager), amount1);
+        _approveTokensForLP(token0, token1, amount0, amount1);
         // Create new Liquidity Pool
         (liquidityPool, tokenId) = _createLP(positionManager, token0, token1, amount0, amount1, newSqrtPriceX96, party.lpFee, _getTicks(token0, party.lpTicks));
 
@@ -53,5 +31,46 @@ abstract contract BNBPartyLiquidity is BNBPartyLiquidityHelper {
         _unwrapAndSendBNB(recipient, unwrapAmount);
         // burn meme tokens
         _burnMemeToken(token0 == address(WBNB) ? token1 : token0);
+    }
+
+    function _approveTokensForLP(
+        address token0,
+        address token1,
+        uint256 amount0,
+        uint256 amount1
+    ) internal {
+        IERC20(token0).safeIncreaseAllowance(address(positionManager), amount0);
+        IERC20(token1).safeIncreaseAllowance(address(positionManager), amount1);
+    }
+
+    function _calculateAmountsAndPrice(
+        address token0,
+        address token1,
+        uint256 amount0,
+        uint256 amount1,
+        uint256 unwrapAmount,
+        uint160 sqrtPriceX96,
+        uint128 liquidity
+    ) internal returns (uint160 newSqrtPriceX96, uint256, uint256) {
+        if (token0 == address(WBNB)) {
+            amount0 -= unwrapAmount;
+            isTokenTargetReached[token1] = true;
+            newSqrtPriceX96 = sqrtPriceCalculator.getNextSqrtPriceFromAmount0RoundingUp(
+                    sqrtPriceX96,
+                    liquidity,
+                    unwrapAmount,
+                    false
+                );
+        } else {
+            amount1 -= unwrapAmount;
+            isTokenTargetReached[token0] = true;
+            newSqrtPriceX96 = sqrtPriceCalculator.getNextSqrtPriceFromAmount1RoundingDown(
+                    sqrtPriceX96,
+                    liquidity,
+                    unwrapAmount,
+                    false
+                );
+        }
+        return (newSqrtPriceX96, amount0, amount1);
     }
 }
